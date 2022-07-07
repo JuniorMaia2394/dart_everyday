2f0adf9fc6648ce24d55ff71e663e19ad3ebada9
JuniorMaia2394 <maiajr2394@gmail.com>
2022-07-06 21:08:27 -0300

    maps

---
+++ Map.dart
@@ -0,0 +1,12 @@
+main() {
+  var maps = {
+    "nome": "Junior Maia",
+    "idade": 27,
+    "cidade": "Mossoró",
+    "estado": "Paraná",
+  };
+
+  print(maps);
+  maps['nome'] = "João Neves";
+  print(maps['nome']);
+}
