ef319b05338e0573e8ad4d00802ddb237a54cb2f
Junior Maia <maiajr2394@gmail.com>
2023-03-02 10:03:16 -0300

    loopFor, fix compras

--- Compras.dart
+++ Compras.dart
@@ -27,16 +27,17 @@ main() {
         removerProduto(list_compras);
       }
     } else if (input_opcao == '3') {
+      print("\x1B[2J\x1B[0;0H");
       if (list_compras.length == 0) {
         print('LISTA VAZIA!!! \n');
         print('=====================================');
       } else {
-        print('LISTA DE PRODUTO(s): ');
+        print(list_compras.length > 1 ? 'LISTA DE PRODUTOS:' : 'PRODUTO:');
         imprimirProdutos(list_compras);
       }
     } else if (input_opcao == '4') {
+      print("\x1B[2J\x1B[0;0H");
       print('FINALIZANDO COMPRAS.... \n');
-      print('=====================================');
 
       list_compras.length == 0
           ? print('LISTA DE COMPRAS VAZIA!!\n')
@@ -44,6 +45,7 @@ main() {
 
       imprimirProdutos(list_compras);
       print('QUANTIDADE:  ' + list_compras.length.toString() + '\n');
+      print('=====================================');
       condicao = false;
     } else {
       print(' OPÇÃO INVÁLIDA!! \n');
@@ -54,21 +56,28 @@ main() {
 
 imprimirProdutos(list_compras) {
   for (var i = 0; i < list_compras.length; i++) {
-    print(list_compras[i].toUpperCase() + '\n');
+    print("\x1B[2J\x1B[0;0H");
+    print(list_compras.length > 1
+        ? 'PRODUTOS:\n' + list_compras.toString().toUpperCase() + '\n'
+        : 'PRODUTO:\n' + list_compras[i].toUpperCase() + '\n');
   }
 }
 
 adicionarProduto(list_compras) {
-  print('ADICONE O  PRODUTO: ');
+  print("\x1B[2J\x1B[0;0H");
+  print('ADICIONE O PRODUTO: ');
   String? input_produto = stdin.readLineSync();
   list_compras.add(input_produto!);
+  print('PRODUTO ADICIONADO COM SUCESSO!');
   print('=====================================');
   // print("\x1B[2J\x1B[0:0H");
 }
 
 removerProduto(list_compras) {
+  print("\x1B[2J\x1B[0;0H");
   print('REMOVA O PRODUTO: ');
   String? input_produto_remove = stdin.readLineSync();
   list_compras.remove(input_produto_remove);
+  print('PRODUTO REMOVIDO COM SUCESSO!');
   print('=====================================');
 }
---
+++ LoopFor.dart
@@ -0,0 +1,11 @@
+main() {
+  // for (var i = 0; i < 10; i++) {
+  //   print("Olá, mundo!");
+  // }
+
+  var names = ['José', 'Antonio', 'Francisca', 'Maria', 'Flinstones'];
+
+  for (var i = 0; i < names.length; i++) {
+    print(names[i]);
+  }
+}
